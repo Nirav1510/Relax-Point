9e8c7c4c758bba306efbb30e34da4f6a96c0c076
Nirav <gusainirav1@gmail.com>
2021-08-12 11:56:50 +0530

    working on bottom nav-bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React from "react";re
 import './App.css'
 import Header from "./components/Header/Header";
+import MainNav from "./components/MainNav";
 
 const App = () => {
   return (
     <React.Fragment>
       <Header />
       <div className="app">random</div>
+      <MainNav/>
     </React.Fragment>
   );
 };
