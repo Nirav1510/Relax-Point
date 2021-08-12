628ae73dd5956fa918278dcd5e1e1e7bd77b3189
Nirav <gusainirav1@gmail.com>
2021-08-12 12:22:03 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from "react";re
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Container } from "@material-ui/core";
 import './App.css'
 import Header from "./components/Header/Header";
 import MainNav from "./components/MainNav";
 
 const App = () => {
   return (
-    <React.Fragment>
+    <BrowserRouter>
       <Header />
-      <div className="app">random</div>
+      <div className="app">
+        <Container>
+          <Switch>
+            <Route path="/" component={Trending} exact />
+            <Route path="/movies" component={Movies} />
+            <Route path="/series" component={Series} />
+            <Route path="/search" component={Search} />
+          </Switch>
+        </Container>
+      </div>
       <MainNav/>
-    </React.Fragment>
+    </BrowserRouter>
   );
 };
 
