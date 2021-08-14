b0f15bd3d623894639e6c179070b4de73e0f96fd
Nirav <gusainirav1@gmail.com>
2021-08-14 23:39:51 +0530

    added badge for rating

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './Card.css'
 import { img_300, unavailable } from "../../config/config";
+import { Badge } from "@material-ui/core";
 
 const Card = ({ id, poster, title, date, media_type, vote_average }) => {
   return (
     <div className="media">
+      <Badge badgeContent={vote_average} color={(vote_average>7) ? "primary" : "secondary"} />
       <img className="poster" src={poster ? `${img_300}/${poster}` : unavailable} alt={title} />
       <b>{title}</b>
       <span className="sunTitle">
