87e7952ddbc63e0d53fd0a9192b33880d35f898d
Nirav <gusainirav1@gmail.com>
2021-08-15 00:46:05 +0530

    Working on Movie page

--- src/pages/Movies/Movies.js
+++ src/pages/Movies/Movies.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Movies = () => {
-    return (
-        <div>
-            <span className="pageTitle">Movies</span>
-        </div>
-    )
-}
+    const [page, setPage] = useState(1);
 
-export default Movies
+  const fetchMovies = async () => {
+    const { data } = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`);
+  };
+
+  return (
+    <div>
+      <span className="pageTitle">Movies</span>
+    </div>
+  );
+};
+
+export default Movies;
