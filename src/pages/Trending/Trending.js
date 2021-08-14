7fd7c8127bc13741f604070cdb8b1f14195ac5ea
Nirav <gusainirav1@gmail.com>
2021-08-14 20:56:09 +0530

    working on card received from api

--- src/pages/Trending/Trending.js
+++ src/pages/Trending/Trending.js
@@ -1,18 +1,27 @@
-import axios from 'axios'
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Trending = () => {
+  const [content, setContent] = useState([]);
 
-    const fetchTrending = async ()=> {
-        const {data} = await axios.get(`
-        https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.REACT_APP_API_KEY}`)
-    }
+  const fetchTrending = async () => {
+    const { data } = await axios.get(`
+        https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.REACT_APP_API_KEY}`);
+    setContent(data.result);
+  };
 
-    return (
-        <div>
-            <span className="pageTitle">Trending</span>
-        </div>
-    )
-}
+  useEffect(() => {
+    fetchTrending();
+  }, []);
 
-export default Trending
+  return (
+    <div>
+      <span className="pageTitle">Trending</span>
+      <div className="trending">
+        {content && content.map((c) => console.log(c))}
+      </div>
+    </div>
+  );
+};
+
+export default Trending;
