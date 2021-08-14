0f92ffed3731c1861c2e1790cca4747ef3bd8c76
Nirav <gusainirav1@gmail.com>
2021-08-14 23:28:02 +0530

    added style and minor changes

--- src/pages/Trending/Trending.js
+++ src/pages/Trending/Trending.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Card from "../../components/Card/Card";
 
 const Trending = () => {
   const [content, setContent] = useState([]);
 
   const fetchTrending = async () => {
-    const { data } = await axios.get(`
-        https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.REACT_APP_API_KEY}`);
-    setContent(data.result);
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`
+    );
+    //console.log(data.results);
+    setContent(data.results);
   };
 
   useEffect(() => {
@@ -17,8 +20,19 @@ const Trending = () => {
   return (
     <div>
       <span className="pageTitle">Trending</span>
-      <div className="trending">
-        {content && content.map((c) => console.log(c))}
+      <div className="flex justify-around flex-wrap">
+        {content &&
+          content.map((c) => (
+            <Card
+              key={c.id}
+              id={c.id}
+              poster={c.poster_path}
+              title={c.title || c.name}
+              date={c.first_air_date || c.release_date}
+              media_type={c.media_type}
+              vote_average={c.vote_average}
+            />
+          ))}
       </div>
     </div>
   );
