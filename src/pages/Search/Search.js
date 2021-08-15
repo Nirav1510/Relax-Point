de8b46b1db1ff1c699109802fbf39966d7c4f206
Nirav <gusainirav1@gmail.com>
2021-08-15 12:52:20 +0530

    Working on search page

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import {
+    Button,
+    createTheme,
+    Tab,
+    Tabs,
+    TextField,
+    ThemeProvider,
+  } from "@material-ui/core";
+
+
 
 const Search = () => {
+    const [type, setType] = useState(0)
+
     return (
         <div>
-            <span className="pageTitle">Search</span>
+            <ThemeProvider>
+
+            </ThemeProvider>
         </div>
     )
 }
