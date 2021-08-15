d8a7177f2a2ccb9b5359776f6d5bd08943fc503d
Nirav <gusainirav1@gmail.com>
2021-08-15 17:08:23 +0530

    Implement Modal in card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,14 +1,21 @@
 import React from "react";
-import './Card.css'
+import "./Card.css";
 import { img_300, unavailable } from "../../config/config";
 import { Badge } from "@material-ui/core";
-import ContentModal from '../ContentModal/ContentModal'
+import ContentModal from "../ContentModal/ContentModal";
 
 const Card = ({ id, poster, title, date, media_type, vote_average }) => {
   return (
-    <ContentModal>
-      <Badge badgeContent={vote_average} color={(vote_average>7) ? "primary" : "secondary"} />
-      <img className="poster" src={poster ? `${img_300}/${poster}` : unavailable} alt={title} />
+    <ContentModal media_type={media_type} id={id}>
+      <Badge
+        badgeContent={vote_average}
+        color={vote_average > 7 ? "primary" : "secondary"}
+      />
+      <img
+        className="poster"
+        src={poster ? `${img_300}/${poster}` : unavailable}
+        alt={title}
+      />
       <b className="title">{title}</b>
       <span className="subTitle">
         {media_type === "tv" ? "TV-Series" : "Movie"}
