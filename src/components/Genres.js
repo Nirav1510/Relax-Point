fcf153c03768baca2b50b20361f1a6a02990dc80
Nirav <gusainirav1@gmail.com>
2021-08-15 12:26:10 +0530

    added fuctionality to Chip

--- src/components/Genres.js
+++ src/components/Genres.js
@@ -10,6 +10,19 @@ const Genres = ({
   type,
   setPage,
 }) => {
+
+  const handleAdd = (genre) => {
+    setSelectedGenres([...selectedGenres, genre]);
+    setGenres(genres.filter((g) => g.id !== genre.id)); //reoving from genres array
+    setPage(1);
+  }
+
+  const handleRemove = (genre) => {
+    setSelectedGenres(selectedGenres.filter((s) => s.id !==genre.id))
+    setGenres([...genres, genre])
+    setPage(1);
+  }
+
   const fetchGenres = async () => {
     const { data } = await axios.get(
       `https://api.themoviedb.org/3/genre/${type}/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
@@ -28,14 +41,25 @@ const Genres = ({
 
   return (
     <div className="mx-1 my-0">
-      {genres.map((genre) => (
+      {selectedGenres && selectedGenres.map((genre) => (
+        <Chip
+          style={{ margin: 2 }}
+          label={genre.name}
+          key={genre.id}
+          color="primary"
+          clickable
+          size="small"
+          onDelete={() => handleRemove(genre)}
+        />
+      ))}
+      {genres && genres.map((genre) => (
         <Chip
           style={{ margin: 2 }}
           label={genre.name}
           key={genre.id}
           clickable
           size="small"
-          //onClick={() => handleAdd(genre)}
+          onClick={() => handleAdd(genre)}
         />
       ))}
     </div>
