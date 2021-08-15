a8cbbf24707048976708e0ff5e349ce936d399b5
Nirav <gusainirav1@gmail.com>
2021-08-15 17:51:00 +0530

    Name changed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div>
       <span className="header" onClick={() => window.scroll(0, 0)}>
-        Movie Masala🎬
+        Relax Point🎬
       </span>
     </div>
   );
