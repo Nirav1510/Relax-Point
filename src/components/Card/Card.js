b60e285c579c0369d9904e48938d676765e894bb
Nirav <gusainirav1@gmail.com>
2021-08-15 16:25:59 +0530

    Working on Modal of a card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,10 +2,11 @@ import React from "react";
 import './Card.css'
 import { img_300, unavailable } from "../../config/config";
 import { Badge } from "@material-ui/core";
+import ContentModal from '../ContentModal/ContentModal'
 
 const Card = ({ id, poster, title, date, media_type, vote_average }) => {
   return (
-    <div className="media">
+    <ContentModal>
       <Badge badgeContent={vote_average} color={(vote_average>7) ? "primary" : "secondary"} />
       <img className="poster" src={poster ? `${img_300}/${poster}` : unavailable} alt={title} />
       <b className="title">{title}</b>
@@ -13,7 +14,7 @@ const Card = ({ id, poster, title, date, media_type, vote_average }) => {
         {media_type === "tv" ? "TV-Series" : "Movie"}
         <span className="subTitle">{date}</span>
       </span>
-    </div>
+    </ContentModal>
   );
 };
 
